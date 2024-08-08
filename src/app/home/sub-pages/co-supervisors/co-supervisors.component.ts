2bf8d2e9fd9af68dc6fa2ead7770fd58975958c8
Amer Absi <Amerabsiiiiii@gmail.com>
2024-08-08 17:44:11 +0300

    Co-Supervisor functionality

--- src/app/home/sub-pages/co-supervisors/co-supervisors.component.ts
+++ src/app/home/sub-pages/co-supervisors/co-supervisors.component.ts
@@ -5,6 +5,29 @@ import { Component } from '@angular/core';
   templateUrl: './co-supervisors.component.html',
   styleUrls: ['./co-supervisors.component.scss']
 })
+
 export class CoSupervisorsComponent {
+  isPersonnelSectionVisible: boolean = false;
+  isModalVisible: boolean = false;
+  // Set the visibility of the personnel section to true
+  showPersonnelSection(): void {
+    this.isPersonnelSectionVisible = true;
+  }
+
+  isInputsFieldSetVisible: boolean = false;  // Controls the visibility of the inputs field set
+
+  toggleInputsFieldSet() {
+    this.isInputsFieldSetVisible = true; // Show the inputs field set
+  }
+  showModal() {
+    this.isModalVisible = true;
+    
+  }
+ 
+  hideModal() {
+    this.isModalVisible = false;
+    
+  }
+  
 
 }
