39b1f7ad64af6d83f320b3672d45fa22fc33e52a
Yazan <yazanabdullah705@gmail.com>
2024-11-06 12:39:10 +0300

    firm detials

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,7 +1,8 @@
 export const environment = {
     production: false,
     location: 'qa',
-    API_URL: 'https://localhost:7091',  // Your local API
+    //API_URL: 'https://localhost:7091',  // Your local API
+    API_URL:'app-backend-bizapps-int-dev-qc-001-f2b3cdfsdpfgbqgu.qatarcentral-01.azurewebsites.net',
     SHAREPOINT_API_URL: 'https://localhost:7163',
     APIs_PREFIX: 'api',
     pageSize: 5
